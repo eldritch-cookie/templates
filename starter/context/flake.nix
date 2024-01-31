de9a5d762bbf23ad655fe595461dd489e8a2314a
Eldritch Cookie <eldritch.cookie@disroot.org>
2024-01-31 11:11:05 -0300

    update templates

---
+++ starter/context/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.pre-commit-hooks-nix.flakeModule
+        inputs.devshell.flakeModule;
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        treefmt.programs = {
+          alejandra.enable = true;
+          stylua.enable = true;
+        };
+        treefmt.settings.formatter = {
+          texfmt = {
+            includes = "**/*.tex";
+            command = "texfmt";
+          };
+        };
+        treefmt.projectRootFile = "flake.nix";
+        pre-commit.settings.hooks = {
+          treefmt.enable = true;
+          typos.enable = true;
+        };
+        devshells.default = {
+          packages = with pkgs; [
+            texlive.scheme-context
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
